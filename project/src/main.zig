3a6e5109acb650ec97866d49f50f998daeb6b7cd
8scarletrose813 <8scarlet.rose813@gamil.com>
2026-03-02 20:08:25 +0200

    Zig for me started

--- project/src/main.zig
+++ project/src/main.zig
@@ -1,7 +1,13 @@
 const std = @import("std");
 
 const print = std.debug.print;
-
+var a  = .{"l","a","b","y","b","y"};
+const year: i16= 18;
+var kilo: f32 = 44.5;
+var see: f32 = -3.25;
 pub fn main() void {
+	print("My year ago {}\n", .{year});
+	print("My kilo{}, my see {}, my year ago{}\n", .{kilo, see, year});
 	print("Hello world!\n", .{});
+	print("My frause: {}\n", .{a});
 }
