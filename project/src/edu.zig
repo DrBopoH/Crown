641d74a2a4c9bbb9e9fe5c09d90c79c8df354d53
DrBopoH <lecrowz666@gmail.com>
2026-03-05 01:20:38 +0200

    fix array of string to bytearray from correcly outputing in to command line in string-like look, not a array of bytes

--- project/src/edu.zig
+++ project/src/edu.zig
@@ -2,15 +2,15 @@ const std = @import("std");
 
 const print = std.debug.print;
 
-var a  = .{"l","a","b","y","b","y"};
+var a  = [_]u8{'l','a','b','y','b','y'};
 const year: i16= 18;
 var kilo: f32 = 44.5;
 var see: f32 = -3.25;
 
 pub fn main() void {
-	print("Hello, I`m begginer!\n", .{});
+	print("Hello, I'm begginer!\n", .{});
 
 	print("My year ago {}\n", .{year});
 	print("My kilo{}, my see {}, my year ago{}\n", .{kilo, see, year});
-	print("My frause: {}\n", .{a});
+	print("My frause: {s}\n", .{a});
 }
--- project/src/main.zig
+++ project/src/main.zig
@@ -4,8 +4,9 @@ const edu = @import("edu.zig");
 //STD Lib
 const std = @import("std");
 
-
+const print = std.debug.print;
 
 pub fn main() void {
+	print("Created at Android 16 Termux 3.13 Helix\n", .{});
 	edu.main();
 }
